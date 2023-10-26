5671a9d223c97e7b0cd309ad7fea8ed2a466dd6a
kaserun <kaserun@Precision-5530>
2023-10-26 10:24:02 +0200

    Package tp04 créé avec la classe Conditionnelle1

---
+++ src/tp04/Conditionnelle1.java
@@ -0,0 +1,22 @@
+package tp04;
+/**
+ * La classe Conditonnelle1 contient uniquement une méthode main qui à partir de 3 variables p1=10, p2=9 et p3=-1, assigne
+ * à p4 la valeur maximale
+ */
+public class Conditionnelle1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p1 = 10, p2 = 9, p3 = -1;
+		int p4;
+		if (p1 > p2) {
+			if (p1 > p3) p4 = p1;
+			else p4 = p3;
+		}
+		else {
+			if (p2>p3) p4 = p2;
+			else p4 = p3;
+		}
+		System.out.println(p4);
+	}
+}
