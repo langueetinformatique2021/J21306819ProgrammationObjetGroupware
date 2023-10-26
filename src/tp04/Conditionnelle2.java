ba40a2c85cc24e629453699bd1458100cc5c80a6
kaserun <kaserun@Precision-5530>
2023-10-26 10:35:08 +0200

    Package Conditionnelle2 créée et testée

--- src/tp04/Conditionnelle2.java
+++ src/tp04/Conditionnelle2.java
@@ -1,20 +1,20 @@
 package tp04;
 /**
  * La classe Conditonnelle1 contient uniquement une méthode main qui à partir de 3 variables p1=10, p2=9 et p3=-1, assigne
- * à p4 la valeur maximale pour ensuite l’afficher dans la console
+ * à p4 la valeur minimale pour ensuite l’afficher dans la console
  */
-public class Conditionnelle1 {
+public class Conditionnelle2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int p1 = 10, p2 = 9, p3 = -1;
 		int p4;
-		if (p1 > p2) {
-			if (p1 > p3) p4 = p1;
+		if (p1 < p2) {
+			if (p1 < p3) p4 = p1;
 			else p4 = p3;
 		}
 		else {
-			if (p2>p3) p4 = p2;
+			if (p2<p3) p4 = p2;
 			else p4 = p3;
 		}
 		System.out.println(p4);
