507ffb34927186295357c7a90c74e53b567df73c
kaserun <kaserun@Precision-5530>
2023-11-23 10:32:31 +0100

    Création de la classe Félin dans le package tp06

---
+++ src/tp06/Félin.java
@@ -0,0 +1,34 @@
+package tp06;
+
+/**
+ * Classe de création et de gestion de félins
+ */
+public class Félin extends Animal{
+	// ajout d’attributs propres à la classe
+	protected boolean domestique = false;
+	
+	/**
+	 * Constructeur de la classe Félin
+	 * @param type nom de l’espèce
+	 */
+	public Félin(String type) {
+		super(type, 4);
+	}
+	
+	/**
+	 * Présentation des caractéristiques de l’animal dans la console
+	 */
+	public void présente() {
+		super.présente();
+		
+		String etat = (domestique) ? "domestique": "sauvage";
+		System.out.println("Je sui vraiment un animal " + etat);
+	}
+	
+	/**
+	 * Cri du félin
+	 */
+	public void crie() {
+		System.out.println("Miaou");
+	}
+}
