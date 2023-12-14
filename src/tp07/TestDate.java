75b601c417bed41b3938d549b6e1ee990b9ab79c
kaserun <kaserun@Precision-5530>
2023-12-14 11:29:56 +0100

    Package tp09 créé

--- src/tp07/TestDate.java
+++ src/tp07/TestDate.java
@@ -2,16 +2,21 @@ package tp07;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class TestDate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		GregorianCalendar gregDate = new GregorianCalendar();
 		Date date = new Date();
 		System.out.println("Date avec la classe GregorianCalendar : " + gregDate.getTime());
 		//System.out.println("Date avec la classe Date : " + date.getDate() + "/" + (date.getMonth() + 1) + "/" + (date.getYear() + 1900) + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
 		System.out.println("Date avec la classe Date : " + date.toString());
+		
+		String str = sc.nextLine();
+		System.out.println("Date et heure : " + gregDate.getTime());
 	}
 
 }
